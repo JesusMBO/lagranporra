dae24a7aa6c9747fecf8fed06c3d6ebd250c38f5
JesusMBO <jesusmbo@gmail.com>
2026-05-28 20:57:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxC2NRC_4lQudSTGy_4oJJFxMQuSMz_b66GIJB3lksjdmOHFi8MTsEm_nmtTSb_OBIN/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyVj5EFLmZkUsj7Dd48lww0ZCkIl0LcJ4F1VAxiX2n9qYhsY-atuLe2V4bEr_b76_QN/exec";
